d7d034e31e69dff1dc3ba628693db3972b5c8458
Cheng Jialuo <56298659+cheng123-123ng@users.noreply.github.com>
2023-05-07 11:22:55 +0800

    Delete new_project directory

--- new_project/plot_picture/plot_trajectory.py
+++
@@ -1,6 +0,0 @@
-from plot_picture.render import render
-import numpy as np
-
-f = 'D:\\桌面\\课程\\毕业设计\\毕业论文\\new_project\\result\\trajectory_1uav_2ucar_train50{}.npy'
-trajectory = np.load(f,allow_pickle=True).item()
-render(trajectory,3,f'D:\\桌面\\课程\\毕业设计\\毕业论文\\照片与数据集\\trajectory_1uav_2ucar_train50.gif')
